2b98e38840255e16db79565aaedff7f923d79dd2
Oleh <oleh.tsarevych@gmail.com>
2023-06-03 17:16:19 +0300

    Reviews
    Aded headings reviews card, folder-reviews with photo , js  sviper

--- src/js/swipper.js
+++ src/js/swipper.js
@@ -1,1 +1,36 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
 
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
