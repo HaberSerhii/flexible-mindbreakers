8274c69a45990955464a2bb62245df6d40f1db83
Oleh <oleh.tsarevych@gmail.com>
2023-06-04 13:05:00 +0300

    reviews
    Aded headings reviews card, folder-reviews with photo , js sviper

--- src/js/slow-scroll.js
+++ src/js/slow-scroll.js
@@ -1,14 +1,14 @@
-const anchors = document.querySelectorAll('a[href*="#"]')
+const anchors = document.querySelectorAll('a[href*="#"]');
 
 for (let anchor of anchors) {
   anchor.addEventListener('click', function (e) {
-    e.preventDefault()
-    
-    const blockID = anchor.getAttribute('href').substr(1)
-    
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute('href').substr(1);
+
     document.getElementById(blockID).scrollIntoView({
       behavior: 'smooth',
-      block: 'start'
-    })
-  })
+      block: 'start',
+    });
+  });
 }
