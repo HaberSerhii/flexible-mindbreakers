a886a3354488798dc35160055067132553c17d9f
AndrewOPP <a.vyblov2003@gmail.com>
2023-06-05 19:54:02 +0300

    100% done

--- src/js/buy-now-modal.js
+++ src/js/buy-now-modal.js
@@ -31,3 +31,70 @@
     
 })();
 
+
+
+// Buy now select
+const item1 = document.getElementById('item-first');
+const item = document.getElementById('item-second');
+const itemm = document.getElementById('item-third');
+const item3 = document.getElementById('item-forth');
+const item4 = document.getElementById('item-fifth');
+const item5 = document.getElementById('item-sixth');
+
+
+item1.addEventListener('click', function() {
+
+  this.classList.toggle('selected-buy-now');
+});
+item.addEventListener('click', function() {
+
+  this.classList.toggle('selected-buy-now');
+});
+itemm.addEventListener('click', function() {
+
+  this.classList.toggle('selected-buy-now');
+});
+item3.addEventListener('click', function() {
+
+  this.classList.toggle('selected-buy-now');
+});
+item4.addEventListener('click', function() {
+
+  this.classList.toggle('selected-buy-now');
+});
+item5.addEventListener('click', function() {
+
+  this.classList.toggle('selected-buy-now');
+});
+
+const numericInput = document.getElementById('personal-tell-second');
+
+// Добавляем обработчик события "input" для поля ввода
+numericInput.addEventListener('input', function() {
+  // Удаляем все нецифровые символы, оставляя только цифры
+  this.value = this.value.replace(/\D/g, '');
+});
+
+const numericInputе = document.getElementById('personal-card-second');
+
+// Добавляем обработчик события "input" для поля ввода
+numericInputе.addEventListener('input', function() {
+  // Удаляем все нецифровые символы, оставляя только цифры
+  this.value = this.value.replace(/\D/g, '');
+});
+
+const numericInpu = document.getElementById('personal-tell');
+
+// Добавляем обработчик события "input" для поля ввода
+numericInpu.addEventListener('input', function() {
+  // Удаляем все нецифровые символы, оставляя только цифры
+  this.value = this.value.replace(/\D/g, '');
+});
+
+const numericInp = document.getElementById('personal-card');
+
+// Добавляем обработчик события "input" для поля ввода
+numericInp.addEventListener('input', function() {
+  // Удаляем все нецифровые символы, оставляя только цифры
+  this.value = this.value.replace(/\D/g, '');
+});
